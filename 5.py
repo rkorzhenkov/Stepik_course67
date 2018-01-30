0409dd123c0fde1fa8b021771525365c5a176a76
Роман Корженков < >
2018-01-30 22:27:22 +0300

    Пятая задача

---
+++ 5.py
@@ -0,0 +1,20 @@
+"""Подобрать нужное окончание к слову при заданном значении """
+
+s = int (input())
+n1 =" программистов"
+n2 =" программист"
+n3 =" программиста"
+if  s>=0:
+  if s==0:
+    print(str(s) + n1)
+    
+  elif s%100<=20 and  s%100>=10  :
+    print(str(s) + n1)
+    
+  elif s%10==1:
+    print(str(s) + n2)
+  
+elif s%10>=2 and s%10<=4:
+    print(str(s) + n3)
+  else:
+    print(str(s) + n1)
