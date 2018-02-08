09710eb5c7de7e315a8a532ea582ffbf6614e655
Роман Корженков < >
2018-02-08 17:22:17 +0300

    Седьмая задача циклы

--- 7.py
+++ 7.py
@@ -1,11 +1,15 @@
-n=int(input())
-m=[]
+"""Вывести последовательность так 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 ... -на вход дается длина массива"""
+
+
+n = int(input())
+m = []
 for i in range(n):
-    j=0
-    while j<i+1:
-        m.append(i+1)
-        j=j+1
-    if len(m)>n: break
-m=m[0:n]
+    j = 0
+    while j < i + 1:
+        m.append(i + 1)
+        j = j + 1
+    if len(m) > n: 
+        break
+m = m[:n]
 for i in m:
     print(i)
